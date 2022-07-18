0766cd8c3ce2e21ac8338f7098badc4daa642fbf
Ammar Ratnani <ammrat13@gmail.com>
2022-07-18 00:28:24 -0400

    Changed default SpinalConfig

--- hdl/src/main/scala/com/github/ammrat13/pynqz2ramtester/Gen.scala
+++ hdl/src/main/scala/com/github/ammrat13/pynqz2ramtester/Gen.scala
@@ -1,19 +1,23 @@
 package com.github.ammrat13.pynqz2ramtester
 
+import spinal.core.ClockDomainConfig
+import spinal.core.LOW
+
 import spinal.core.SpinalConfig
+import spinal.core.Verilog
 
 /** Generates code for [[PynqZ2RamTester]]
   *
-  * Generates `SpinalConfig` from command-line arguments:
-  * {{{
-  * Usage: SpinalCore [options]
-  *
-  *   --vhdl                   Select the VHDL mode
-  *   --verilog                Select the Verilog mode
-  *   -o, --targetDirectory <value>
-  *                            Set the target directory
-  * }}}
+  * Uses a hard-coded `SpinalConfig`. Compiles to Verilog and outputs in
+  * `build/`, and sets the resets to active-low.
   */
 object Gen extends App {
-  SpinalConfig.shell(args).generate(new PynqZ2RamTester)
+  SpinalConfig(
+    mode = Verilog,
+    targetDirectory = "build/",
+    defaultConfigForClockDomains = ClockDomainConfig(
+      resetActiveLevel = LOW,
+      softResetActiveLevel = LOW,
+    ),
+  ).generate(new PynqZ2RamTester)
 }
